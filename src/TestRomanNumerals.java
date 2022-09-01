b972686963cd552612d4f317c35a1c7511970093
Mayonnaisu <toli025@student.kristiania.no>
2022-09-01 10:25:50 +0200

    🔴 RED - Convert to roman numerals

---
+++ src/TestRomanNumerals.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestRomanNumerals {
+
+    @Test
+    void testNumeral1() {
+        assertEquals(convertToRomanNumeral(1), "I");
+    }
+
+    private String convertToRomanNumeral(int i) {
+        return "a";
+    }
+
+
+}
